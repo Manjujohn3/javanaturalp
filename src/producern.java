d68167404a639a889ac48546a7b1fe65ac0834a1
Manjujohn3 <manjujohn301@gmail.com>
2022-11-23 09:53:32 +0530

    naturalnumberproducer

---
+++ src/producern.java
@@ -0,0 +1,33 @@
+import java.util.Properties;
+import java.util.Random;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+public class producern {
+
+
+        public static  void main(String args[])
+        {
+            KafkaProducer producer;
+            String broker ="localhost:9092";
+            String topic ="newEven";
+            Properties props = new Properties();
+            props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,broker);
+            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+            producer = new KafkaProducer(props);
+            Random rand = new Random();
+            while (true)
+                try {
+                    int dataTosend = rand.nextInt(0, 1000);
+                    String val = String.valueOf(dataTosend);
+                    producer.send(new ProducerRecord(topic, val));
+                    Thread.sleep(1000);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+
+        }
+    }
